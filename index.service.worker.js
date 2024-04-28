77370499a490ca698f891c647ebca2f8dbbcec9b
singerbj <singerbj@users.noreply.github.com>
2024-04-28 14:00:00 +0000

    Deploying to gh-pages from @ singerbj/bag-tag@c96f4ceef66e67b21f8b83a273a9969425d534f9 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1714241662|9763584";
+const CACHE_VERSION = "1714312789|10478514";
 const CACHE_PREFIX = "bag-tag-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
